955af3a06592483e284326ebe0ea2009617b33d4
amitgupta847 <amit847.swastic@gmail.com>
2022-09-11 20:40:51 -0400

    first commit

---
+++ ECommerce.Api.Products/Interfaces/IProductsProviders.cs
@@ -0,0 +1,17 @@
+﻿
+using ECommerce.Api.Products.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.Interfaces
+{
+  public interface IProductsProvider
+  {
+    //returning the named TUPLE
+    //Return all the Products
+    Task<(bool IsSuccess, IEnumerable<Product> Products, string ErrorMessage)> GetProductsAsync();
+
+
+    Task<(bool IsSuccess, Product Product, string ErrorMessage)> GetProductAsync(int id);
+  }
+}
---
+++ ECommerce.Api.Products/Profiles/ProductProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.Api.Products.Profiles
+{
+
+
+  //To define mapping between Product model and Product Entity.
+  public class ProductProfile : AutoMapper.Profile
+  {
+    public ProductProfile()
+    {
+      CreateMap<Db.Product, Models.Product>();
+    }
+  }
+}
